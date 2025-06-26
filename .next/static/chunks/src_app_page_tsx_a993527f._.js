d4a37cf1cfc283ef26c9fa5ab94526bf856a72a0
jhonf16071990 <jfsuarezg@hotmail.com>
2025-06-25 23:38:51 -0500

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750912732813

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8431767d._.js",
+  "static/chunks/node_modules_9ace8bbf._.js"
+],
+    source: "dynamic"
+});
